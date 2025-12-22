479473fdb5934df5643c552f762553074dccbb31
Pavel Karpeichyk <pashkar@bk.ru>
2025-12-22 17:22:19 +0300

    update: correct imports

--- Android/src/test/kotlin/driver/widget/WidgetsOperations.kt
+++ Android/src/test/kotlin/driver/widget/WidgetsOperations.kt
@@ -1,10 +1,10 @@
 package driver.widget
 
-import com.codeborne.selenide.Condition
+import com.codeborne.selenide.Condition.visible
 import com.codeborne.selenide.appium.SelenideAppium.`$`
 import io.qameta.allure.Step
 import org.openqa.selenium.By
-import java.time.Duration
+import java.time.Duration.ofSeconds
 
 internal class WidgetsOperations {
   private val widgetLoadTimeoutSeconds: Long = 60L
@@ -12,7 +12,6 @@ internal class WidgetsOperations {
 
   @Step
   fun verifyStartApplicationWidgetOpened() {
-    `$`(introWidgetLocator).shouldBe(Condition.visible, Duration.ofSeconds(widgetLoadTimeoutSeconds))
+    `$`(introWidgetLocator).shouldBe(visible, ofSeconds(widgetLoadTimeoutSeconds))
   }
-
 }
